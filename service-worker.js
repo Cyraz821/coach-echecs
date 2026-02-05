51d792a07295dee7f190580d1500fc987933586d
Cyraz821 <cicou82@gmail.com>
2026-02-05 16:54:01 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -9,13 +9,15 @@ const FILES_TO_CACHE = [
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    )
   );
+  self.clients.claim();
 });
+
